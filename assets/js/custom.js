7c91f0960a837961214365a79d47068810a70692
gregory lewis <eternal191@gmail.com>
2025-11-01 23:12:09 -0400

    test url_4

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -150,7 +150,7 @@
       };
 
       try {
-        const response = await fetch('/api/send-email', {
+        const response = await fetch('https://digitaldimensionz-p4p5s3pzc-eternal191s-projects.vercel.app/api/send-email', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
